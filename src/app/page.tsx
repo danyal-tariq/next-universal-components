2306dc90a8d4d691b493091d42941d04c96a9405
Danyal Tariq <danialtariq43@gmail.com>
2024-06-11 14:00:03 +0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { NavBar } from "./Components/NavBar";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full justify-items-center place-items-center">
+      <NavBar />
+    </div>
+  );
+}
